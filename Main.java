cd5ea029899051847f21f39f8a3ac96d29c938b8
AYUSH HURKAT <120328520+Ayushhurkat22@users.noreply.github.com>
2024-09-06 12:23:16 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		// Instantiate CarRentalGUI without arguments
 		CarRentalGUI gui = new CarRentalGUI();
 		gui.setVisible(true);
 	}
